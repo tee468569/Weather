030b74e20cc560fcf557baecd639f5a69c5d1c06
tenz12345 <207060066+tee468569@users.noreply.github.com>
2026-01-04 12:39:50 +0100

    added apiKey and apiUrl and updated html,css and js

--- src/script.js
+++ src/script.js
@@ -1,9 +1,23 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temp");
+  let temperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+
+  let city = document.querySelector("#city-name");
+
+  city.innerHTML = response.data.city;
+}
+function citySearch(city) {
+  let apiKey = "3fb4403a03669541456bf8otac408501";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 function searchEngine(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#city-input");
-  let h1 = document.querySelector("#city-name");
-  h1.innerHTML = searchInput.value;
+  citySearch(searchInput.value);
 }
 
 let form = document.querySelector("#search-form");
